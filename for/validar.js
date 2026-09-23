60ca01b0be0ee7e962db62bc057c77f888bd1a34
hernan <hernan.perezhiguita.13@gmail.com>
2026-09-23 08:13:04 -0500

    feat: dentro del archivo validar.js se refactoriza el parametro de la funcion mensaje de valido a accesoValido

--- for/validar.js
+++ for/validar.js
@@ -2,8 +2,8 @@ export function validar(usuario, password, usuarioCorrecto, passwordCorrecto){
     return(usuario === usuarioCorrecto && password === passwordCorrecto);
 }
 
-export function mensaje(valido, intentosActuales, intentosMaximos){
-    if (valido) {
+export function mensaje(accesoValido, intentosActuales, intentosMaximos){
+    if (accesoValido) {
         alert(`Bienvenido al sistema`);
     } else if (intentosActuales < intentosMaximos) {
         alert(`Datos incorrectos intento ${intentosActuales} de ${intentosMaximos} intentos`);
